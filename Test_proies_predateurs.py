d6510cdffc04788e34c252c3030757d605d8ef1c
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-04-15 12:38:29 +0200

    Fonctions identiteProies et identitePredateurs

--- Test_proies_predateurs.py
+++ Test_proies_predateurs.py
@@ -1,30 +1,11 @@
-# définir HAUTEUR_CANEVAS + LARGEUR_CANEVAS + NB_CASAS
-
-HAUTEUR_CANEVAS = 500
-LARGEUR_CANEVAS = 500
-NB_CASES = 30
-
+def identiteProies(matrice, x, y):
+    """ Remplit la case de la matrice par le tuple correspondant à l'identité de la proie (son âge, etc...)"""
+    
+    matrice[x][y] = (1, A_PRO)
 
 
-import tkinter as tk
 
-racine = tk.Tk()
-racine.title("Simulation d'un système proies/prédateurs")
-
-canevas = tk.Canvas(racine, height=HAUTEUR_CANEVAS, width=LARGEUR_CANEVAS, bg='white')
-canevas.grid(column=0, row=0)
-
-def creationGrille(NB_CASES):
-    """ Créer une grille carrée de NB_CASES cases de largeur"""
-
-    taille_case = LARGEUR_CANEVAS / NB_CASES
-
-    for k in range(1, (NB_CASES)):
-        # lignes verticales
-        canevas.create_line((k * taille_case), 0, (k * taille_case), HAUTEUR_CANEVAS)
-        # lignes horizontales
-        canevas.create_line(0, (k * taille_case), LARGEUR_CANEVAS, (k * taille_case))
-
-creationGrille(NB_CASES)
-
-racine.mainloop()
+def identitePredateurs(matrice, x, y):
+    """ Remplit la case de la matrice par le tuple correspondant à l'identité du prédateur"""
+    
+    matrice[x][y] = (2, A_PRE, E_PRE)
