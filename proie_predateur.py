39fac3d3fecc1fb98f30664b6f781252a3e0de66
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-04-15 19:32:59 +0200

    Fonction voisinnage

--- proie_predateur.py
+++ proie_predateur.py
@@ -90,6 +90,65 @@ def couleurCases(matrice):
                 canevas.itemconfigure(case, fill="DarkOrange1")
 
 
+def voisinnage(i, j, matrice):
+    """ Créer une liste contenant le type des individus voisins d'un animal en position (i, j)"""
+
+    # l'ordre des voisins dans la liste est défini de gauche à droite puis de haut en bas
+    l = []
+
+    if i==0 and j==0 :
+        l.append(matrice[i][j+1][0])
+        l.append(matrice[i+1][j][0])
+        l.append(matrice[i+1][j+1][0])
+    elif i==0 and (j==len(matrice)-1):
+        l.append(matrice[i][j-1][0])
+        l.append(matrice[i+1][j-1][0])
+        l.append(matrice[i+1][j][0])
+    elif (i==len(matrice)-1) and j==0:
+        l.append(matrice[i-1][j][0])
+        l.append(matrice[i-1][j+1][0])
+        l.append(matrice[i][j+1][0])
+    elif (i==len(matrice)-1) and (j==len(matrice)-1):
+        l.append(matrice[i-1][j-1][0])
+        l.append(matrice[i-1][j][0])
+        l.append(matrice[i][j-1][0])
+    elif i==0 and 0<j<(len(matrice)-1):
+        l.append(matrice[i][j-1][0])
+        l.append(matrice[i][j+1][0])
+        l.append(matrice[i+1][j-1][0])
+        l.append(matrice[i+1][j][0])
+        l.append(matrice[i+1][j+1][0])
+    elif (i==len(matrice)-1) and 0<j<(len(matrice)-1):
+        l.append(matrice[i-1][j-1][0])
+        l.append(matrice[i-1][j][0])
+        l.append(matrice[i-1][j+1][0])
+        l.append(matrice[i][j-1][0])
+        l.append(matrice[i][j+1][0])
+    elif (0<i<(len(matrice)-1) and j==0):
+        l.append(matrice[i-1][j][0])
+        l.append(matrice[i-1][j+1][0])
+        l.append(matrice[i][j+1][0])
+        l.append(matrice[i+1][j][0])
+        l.append(matrice[i+1][j+1][0])
+    elif 0<i<(len(matrice)-1) and j==(len(matrice)-1):
+        l.append(matrice[i-1][j-1][0])
+        l.append(matrice[i-1][j][0])
+        l.append(matrice[i][j-1][0])
+        l.append(matrice[i+1][j-1][0])
+        l.append(matrice[i+1][j][0])
+    else :
+        l.append(matrice[i-1][j-1][0])
+        l.append(matrice[i-1][j][0])
+        l.append(matrice[i-1][j+1][0])
+        l.append(matrice[i][j-1][0])
+        l.append(matrice[i][j+1][0])
+        l.append(matrice[i+1][j-1][0])
+        l.append(matrice[i+1][j][0])
+        l.append(matrice[i+1][j+1][0])
+
+    return l
+
+
 ############################################
 ##### Fonctions pour la gestion des populations
 
