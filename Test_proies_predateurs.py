4757f18233b577b4c3b0272fb5a218713290681a
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-04-22 22:53:46 +0200

    Fonctions reproductionPredateurs + coordonneesVoisins

--- Test_proies_predateurs.py
+++ Test_proies_predateurs.py
@@ -1,67 +1,12 @@
+import random as rd
+
+
+
 def predation(matrice):
     """ Si une proie et un prédateur sont sur la même case, alors la proie meure et le prédateur gagne MIAM"""
     
+
     for i in range(len(matrice)):
         for j in range(len(matrice)):
             if matrice[i][j][0] == 2:
-                
-
-
-
-def voisinnage(i, j, matrice):
-    """ Créer une liste contenant le type des individus voisins d'un animal"""
-
-    # l'ordre des voisins dans la liste est défini de gauche à droite puis de haut en bas
-    l = []
-
-    if i==0 and j==0 :
-        l.append(matrice[i][j+1][0])
-        l.append(matrice[i+1][j][0])
-        l.append(matrice[i+1][j+1][0])
-    elif i==0 and (j==len(matrice)-1):
-        l.append(matrice[i][j-1][0])
-        l.append(matrice[i+1][j-1][0])
-        l.append(matrice[i+1][j][0])
-    elif (i==len(matrice)-1) and j==0:
-        l.append(matrice[i-1][j][0])
-        l.append(matrice[i-1][j+1][0])
-        l.append(matrice[i][j+1][0])
-    elif (i==len(matrice)-1) and (j==len(matrice)-1):
-        l.append(matrice[i-1][j-1][0])
-        l.append(matrice[i-1][j][0])
-        l.append(matrice[i][j-1][0])
-    elif i==0 and 0<j<(len(matrice)-1):
-        l.append(matrice[i][j-1][0])
-        l.append(matrice[i][j+1][0])
-        l.append(matrice[i+1][j-1][0])
-        l.append(matrice[i+1][j][0])
-        l.append(matrice[i+1][j+1][0])
-    elif (i==len(matrice)-1) and 0<j<(len(matrice)-1):
-        l.append(matrice[i-1][j-1][0])
-        l.append(matrice[i-1][j][0])
-        l.append(matrice[i-1][j+1][0])
-        l.append(matrice[i][j-1][0])
-        l.append(matrice[i][j+1][0])
-    elif (0<i<(len(matrice)-1) and j==0):
-        l.append(matrice[i-1][j][0])
-        l.append(matrice[i-1][j+1][0])
-        l.append(matrice[i][j+1][0])
-        l.append(matrice[i+1][j][0])
-        l.append(matrice[i+1][j+1][0])
-    elif 0<i<(len(matrice)-1) and j==(len(matrice)-1):
-        l.append(matrice[i-1][j-1][0])
-        l.append(matrice[i-1][j][0])
-        l.append(matrice[i][j-1][0])
-        l.append(matrice[i+1][j-1][0])
-        l.append(matrice[i+1][j][0])
-    else :
-        l.append(matrice[i-1][j-1][0])
-        l.append(matrice[i-1][j][0])
-        l.append(matrice[i-1][j+1][0])
-        l.append(matrice[i][j-1][0])
-        l.append(matrice[i][j+1][0])
-        l.append(matrice[i+1][j-1][0])
-        l.append(matrice[i+1][j][0])
-        l.append(matrice[i+1][j+1][0])
-
-    return l
+                voisins = voisinnage(i, j, matrice)
--- proie_predateur.py
+++ proie_predateur.py
@@ -18,6 +18,8 @@
 
 import tkinter as tk
 
+import random as rd
+
 
 ############################################
 ### Définition des constantes
@@ -91,7 +93,7 @@ def couleurCases(matrice):
 
 
 def voisinnage(i, j, matrice):
-    """ Créer une liste contenant le type des individus voisins d'un animal en position (i, j)"""
+    """ Créer une liste contenant le type des individus voisins d'un animal"""
 
     # l'ordre des voisins dans la liste est défini de gauche à droite puis de haut en bas
     l = []
@@ -149,6 +151,66 @@ def voisinnage(i, j, matrice):
     return l
 
 
+def coordonneesVoisins(i, j, matrice):
+
+    """ Créer une liste contenant les coordonnées des cases voisines d'une case"""
+
+    # l'ordre des voisins dans la liste est défini de gauche à droite puis de haut en bas
+    l = []
+
+    if i==0 and j==0 :
+        l.append((i, j+1))
+        l.append((i+1, j))
+        l.append((i+1, j+1))
+    elif i==0 and (j==len(matrice)-1):
+        l.append((i, j-1))
+        l.append((i+1, j-1))
+        l.append((i+1, j))
+    elif (i==len(matrice)-1) and j==0:
+        l.append((i-1, j))
+        l.append((i-1, j+1))
+        l.append((i, j+1))
+    elif (i==len(matrice)-1) and (j==len(matrice)-1):
+        l.append((i-1, j-1))
+        l.append((i-1, j))
+        l.append((i, j-1))
+    elif i==0 and 0<j<(len(matrice)-1):
+        l.append((i, j-1))
+        l.append((i, j+1))
+        l.append((i+1, j-1))
+        l.append((i+1, j))
+        l.append((i+1, j+1))
+    elif (i==len(matrice)-1) and 0<j<(len(matrice)-1):
+        l.append((i-1, j-1))
+        l.append((i-1, j))
+        l.append((i-1, j+1))
+        l.append((i, j-1))
+        l.append((i, j+1))
+    elif (0<i<(len(matrice)-1) and j==0):
+        l.append((i-1, j))
+        l.append((i-1, j+1))
+        l.append((i, j+1))
+        l.append((i+1, j))
+        l.append((i+1, j+1))
+    elif 0<i<(len(matrice)-1) and j==(len(matrice)-1):
+        l.append((i-1, j-1))
+        l.append((i-1, j))
+        l.append((i, j-1))
+        l.append((i+1, j-1))
+        l.append((i+1, j))
+    else :
+        l.append((i-1, j-1))
+        l.append((i-1, j))
+        l.append((i-1, j+1))
+        l.append((i, j-1))
+        l.append((i, j+1))
+        l.append((i+1, j-1))
+        l.append((i+1, j))
+        l.append((i+1, j+1))
+
+    return l
+
+
 ############################################
 ##### Fonctions pour la gestion des populations
 
@@ -181,6 +243,7 @@ def identiteProies(matrice, x, y):
     
     matrice[x][y] = (1, A_PRO)
 
+    return
 
 
 def identitePredateurs(matrice, x, y):
@@ -219,9 +282,47 @@ def deplacementPredateurs():
     pass
 
 
-def reproductionProies():
-    """ Si 2 proies sont à côté, elles se reproduisent"""
-    pass
+def reproductionProies(matrice):
+    """ Si 2 proies sont à côté, alors elles se reproduisent (on décide que les proies ne 
+    peuvent se reproduire au maximum qu'une seule fois par tour)"""
+    
+    matrice_proies = [[(0,0)]*len(matrice) for b in range(len(matrice))]
+
+
+    # On commence par créer une nouvelle matrice de la même taille que la matrice d'entrée,
+    # qui contient un 1 s'il y a une proie, un 0 sinon (cela simplifie l'utilisation de la matrice)
+    for x in range(len(matrice_proies)):
+        for y in range(len(matrice_proies)):
+            if matrice[x][y][0] == 1:       ## Si c'est une proie
+                matrice_proies[x][y] = (1,0)
+
+
+    # On parcourt notre matrice contenant les informations des positions des proies puis l'on regarde
+    # si une proie est à coté d'une autre ; si c'est le cas, elles se reproduisent
+    for i in range(len(matrice_proies)):
+        for j in range(len(matrice_proies)):
+            if matrice_proies[i][j] == (1,0) :       ## Si c'est une proie
+                voisins = voisinnage(i, j, matrice_proies)
+                voisins_vide = voisinnage(i, j, matrice)
+                if (1 in voisins) and (0 in voisins_vide):        
+                    ## Si la proie est à côté d'une autre proie et qu'il y a une case vide pour la naissance
+                    verif_naissance = 0
+                    while matrice_proies[i][j] == (1,0):        ## Tant que la proie ne s'est pas reproduit
+                        h = rd.randint(0, (len(voisins)-1))
+                        if voisins[h] == 1:      ## Si un voisin pris au hasard est une proie, il y a reproduction
+                            matrice_proies[i][j] = (0,0)        ## La proie ne peut plus se reproduire pendant ce tour          
+                            coord_pro = coordonneesVoisins(i, j, matrice)
+                            matrice_proies[coord_pro[h][0]][coord_pro[h][1]] = 0
+                            while verif_naissance == 0:      ## Tant qu'il n'y a pas eu la naissance 
+                                k = rd.randint(0, (len(voisins_vide)-1))
+                                if voisins_vide[k] == 0:           ## Si la case choisie au hasard est vide
+                                    identiteProies(matrice, coord_pro[k][0], coord_pro[k][1])
+                                    #matrice[coord_pro[k][0]][coord_pro[k][1]] = (1, A_PRO)
+                                    verif_naissance = 1
+                                
+
+    return matrice
+
 
 
 def reproductionPredateurs():
