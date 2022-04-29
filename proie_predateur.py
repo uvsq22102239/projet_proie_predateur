351b541eefca25d1281e1a7780067d8aa83ca153
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2022-04-29 16:50:53 +0200

    modif ageProies

--- proie_predateur.py
+++ proie_predateur.py
@@ -366,7 +366,7 @@ def ageProies(matrice):
     for i in range(len(matrice)):
         for j in range(len(matrice)):
             if matrice[i][j][0] == 1:       # si c'est une proie
-                matrice[i][j][1] -= 1       # son âge diminue de 1
+                matrice[i][j][1] = matrice[i][j][1] - 1       # son âge diminue de 1
 
 
 
